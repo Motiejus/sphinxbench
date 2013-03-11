2c71074041baf2840154391ce84337c9b633bc79
Motiejus Jakštys <motiejus.jakstys@spilgames.com>
2013-03-11 16:07:42 +0100

    FIX remove siteid filter

--- src/sphinx_bin_driver.erl
+++ src/sphinx_bin_driver.erl
@@ -20,8 +20,7 @@ new(_Id) ->
             Q2 = giza_query:host(Q1, Host),
             Q3 = giza_query:port(Q2, Port),
             Q4 = giza_query:limit(Q3, 10),
-            Q5 = giza_query:add_filter(Q4, "siteid", [12]),
-            giza_query:sort_extended(Q5, "total_play_count desc")
+            giza_query:sort_extended(Q4, "total_play_count desc")
     end,
     {ok, {Type, Query}}.
 
